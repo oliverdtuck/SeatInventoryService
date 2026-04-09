6dbd224a9823f7262a0e34365a04c95314c42d0f
Oliver Tuck <oliver@elitecoders.co.uk>
2026-04-09 17:58:44 +0100

    Remove space

--- SeatInventoryService.Domain/Events/SeatHeld.cs
+++ SeatInventoryService.Domain/Events/SeatHeld.cs
@@ -2,5 +2,4 @@ using SeatInventoryService.Domain.Events.Base;
 
 namespace SeatInventoryService.Domain.Events;
 
-
 public record SeatHeld(Guid SeatId, Guid FlightId, Guid PassengerId, DateTime ExpiresAt) : DomainEvent;
